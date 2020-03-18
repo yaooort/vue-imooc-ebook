aa583c99fe17fd9f43bb3decca91ad8ac11983c4
Oort <yaooort@gmail.com>
2020-03-18 13:36:34 +0800

    ti

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ function mock (app, url, data) {
 // 设置这个的作用是打包出来的css,js资源路径不对
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? './' : '/',
+    ? './' : './',
 //  模拟请求
   devServer: {
     before (app) {
